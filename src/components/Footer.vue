a5b011d43b564a95fb334818700814948531cf02
erison <erison@flexpeak.com.br>
2020-08-01 20:38:35 -0400

    [add] css Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,22 @@
 <template>
-  <footer>
+  <footer class="c-footer">
     <p>Erison Alexandre - 2020</p>
   </footer>
 </template>
+
+<style lang="scss">
+@import "@/assets/scss/_variaveis";
+.c-footer {
+  display: flex;
+  width: 100%;
+  background: $azul;
+  height: 70px;
+  padding: 5px;
+  color: white;
+  font-weight: bold;
+  p {
+    margin: auto 0;
+    padding-left: 20px;
+  }
+}
+</style>
