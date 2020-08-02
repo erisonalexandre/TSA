ddad2197c3c2cbdb140ae667d4a32f87db90d49d
erison <erison@flexpeak.com.br>
2020-08-02 00:33:12 -0400

    [add] adicionando header na pagina Cadastro e na pagina Lista

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -1,3 +1,12 @@
 <template>
-  <div></div>
+  <div><Header titulo="Cadastro"></Header></div>
 </template>
+<script>
+import Header from "@/components/Header"
+export default {
+  name: "Cadastro",
+  components: {
+    Header
+  }
+}
+</script>
--- src/views/Lista.vue
+++ src/views/Lista.vue
@@ -1,3 +1,12 @@
 <template>
-  <div></div>
+  <div><Header titulo="Lista"></Header></div>
 </template>
+<script>
+import Header from "@/components/Header"
+export default {
+  name: "Lista",
+  components: {
+    Header
+  }
+}
+</script>
