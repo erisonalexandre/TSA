57775ca0c4954a8092da632f7b49e373b6dd61f6
erison <erison@flexpeak.com.br>
2020-08-01 23:49:26 -0400

    [add] css componente Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="c-header">
+    <div class="container">
+      <h1 class="c-header__titulo">{{ titulo }}</h1>
+    </div>
   </div>
 </template>
 <script>
@@ -13,3 +15,19 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/_variaveis";
+.c-header {
+  background-color: #eff4f9;
+  height: 116.5px;
+  &__titulo {
+    color: #445566;
+    margin: 0;
+    line-height: 116px;
+    font-size: 71px;
+    font-weight: 300;
+    font-family: $font-alice;
+  }
+}
+</style>
