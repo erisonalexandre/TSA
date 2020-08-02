ce82f021e3e7a974e3697e9bbc5ab18c2b5ee4c4
erison <erison@flexpeak.com.br>
2020-08-01 20:57:22 -0400

    [add] componente Header

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    titulo: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ tests/unit/header.spec.js
@@ -0,0 +1,23 @@
+import { mount } from "vue-test-utils"
+import Header from "../../src/components/Header"
+import expect from "expect"
+
+describe("Testando Header", () => {
+  let header
+
+  beforeEach(() => {
+    header = mount(Header, {
+      propsData: {
+        titulo: "Cadastro"
+      }
+    })
+  })
+
+  it("Testando props", () => {
+    expect(header.props().titulo).toBe("Cadastro")
+  })
+
+  it("Testando conteudo", () => {
+    expect(header.html()).toContain("<h1>Cadastro</h1>")
+  })
+})
